e86ee7951d41761df9811e71c955bb4fc485eb29
eminakurtic <57722363+eminakurtic@users.noreply.github.com>
2020-02-17 14:32:07 +0100

    Add files via upload

---
+++ leno/includes/body.html.php
@@ -0,0 +1,89 @@
+<body data-spy="scroll" data-target=".fixed-top">
+    
+    <!-- Preloader -->
+	<div class="spinner-wrapper">
+        <div class="spinner">
+            <div class="bounce1"></div>
+            <div class="bounce2"></div>
+            <div class="bounce3"></div>
+        </div>
+    </div>
+    <!-- end of preloader -->
+    
+
+    <!-- Navigation: Start -->
+        <?php require 'includes/nav.html.php'; ?>
+    <!-- end of navigation -->
+
+
+    <!-- Header: Start -->
+        <?php require 'includes/header.html.php'; ?>
+    <!-- end of header -->
+
+
+    <!-- Testimonials: Start -->
+        <?php require 'includes/testimonials.html.php'; ?>
+    <!-- end of testimonials -->
+    
+
+    <!-- Features: Start -->
+        <?php require 'includes/features.html.php'; ?>
+    <!-- end of features -->
+
+
+    <!-- Video: Start -->
+        <?php require 'includes/video.html.php'; ?>
+    <!-- end of video -->
+
+
+    <!-- Details 1: Start -->
+        <?php require 'includes/details1.html.php'; ?>
+    <!-- end of details 1 -->
+
+
+    <!-- Details 2: Start -->
+        <?php require 'includes/details2.html.php'; ?>
+    <!-- end of details 2 -->
+
+
+    <!-- Details Lightboxes -->
+	<!-- Lightbox: Start -->
+        <?php require 'includes/lightbox.html.php'; ?>
+    <!-- end of lightbox -->
+    <!-- end of details lightboxes -->
+
+
+    <!-- Screenshots: Start -->
+        <?php require 'includes/screenshots.html.php'; ?>
+    <!-- end of screenshots -->
+
+
+    <!-- Download: Start -->
+        <?php require 'includes/download.html.php'; ?>
+    <!-- end of download -->
+
+
+    <!-- Statistics: Start -->
+        <?php require 'includes/statistics.html.php'; ?>
+    <!-- end of statistics -->
+
+
+    <!-- Contact: Start -->
+        <?php require 'includes/contact.html.php'; ?>
+    <!-- end of contact -->
+
+
+    <!-- Footer: Start -->
+        <?php require 'includes/footer.html.php'; ?>
+    <!-- end of footer -->
+
+
+    <!-- Copyright: Start -->
+        <?php require 'includes/copyright.html.php'; ?>
+    <!-- end of copyright -->
+    
+    	
+    <!-- Scripts -->
+        <?php require 'includes/scripts.html.php'; ?>
+    <!-- Scripts: End -->
+</body>
